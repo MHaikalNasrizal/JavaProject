cf6b0f99c34b5f1d64b55a6de4f01a4de4d9ae4f
MHaikalNasrizal <haikalnasrizal321@gmail.com>
2022-12-03 16:09:57 +0800

    Main menu for Account v2 Commit

--- JavaProject/src/javaproject/JavaProject.java
+++ JavaProject/src/javaproject/JavaProject.java
@@ -10,45 +10,95 @@ public class JavaProject {
         double interest, balance;
         int accNum;
         int choice = 1;
-        int i =0;
+        int i = 0;
         BankAccount[] account = new BankAccount[50];
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        do{
+        do {
             System.out.println("=== ABC BANK MAIN MENU ===");
             System.out.print("\nSelect : \n 1. Create Account\n 2. Transaction \n Choose (0 to exit the program): ");
             choice = scanner.nextInt();
-                
-                if (choice == 1){
-                    System.out.println("=[Create Account]=");
-                    System.out.print("\nSelect: \n 1. Saving Account \n 2. Current Account\n Choose (0 to exit the program):");
-                    choice = scanner.nextInt();
-                    scanner.nextLine();
-                    if (choice == 1){
-                        System.out.println("Enter your Full name : ");
-                        Fullname = scanner.nextLine();
-                        
-                        accNum = random.nextInt(9999 - 1001 + 1) + 1001;
-                        account[i] = new SavingAccount(Fullname,"Saving Account",accNum);
-                        System.out.println(account[i]);
-                        i++;
-                    } else if (choice == 2){
-                        System.out.println("Enter your Full name : ");
-                        Fullname = scanner.nextLine();
-                        
-                        accNum = random.nextInt(9999 - 1001 + 1) + 1001;
-                        account[i] = new CurrentAccount(Fullname,"Current Account",accNum);
-                        System.out.println(account[i]);
-                        i++;
-                    }
-                } else if (choice ==2 ){
-                    System.out.println("=[Transaction]=");
-                    System.out.print("\nSelect: \n 1. Display \n 2. Deposit \n 3. Withdraw \n 4. Transfer\n Choose (0 to exit the program):");
-                    choice = scanner.nextInt();
+
+            if (choice == 1) {
+                System.out.println("=[Create Account]=");
+                System.out.print("\nSelect: \n 1. Saving Account \n 2. Current Account\n Choose (0 to exit the program):");
+                choice = scanner.nextInt();
+                scanner.nextLine();
+                if (choice == 1) {
+                    System.out.println("Enter your Full name : ");
+                    Fullname = scanner.nextLine();
+
+                    accNum = random.nextInt(9999 - 1001 + 1) + 1001;
+                    account[i] = new SavingAccount(Fullname, "Saving Account", accNum);
+                    System.out.println(account[i]);
+                    i++;
+                } else if (choice == 2) {
+                    System.out.println("Enter your Full name : ");
+                    Fullname = scanner.nextLine();
+
+                    accNum = random.nextInt(9999 - 1001 + 1) + 1001;
+                    account[i] = new CurrentAccount(Fullname, "Current Account", accNum);
+                    System.out.println(account[i]);
+                    i++;
                 }
-            
-        
-        }while (choice != 0);
+            } else if (choice == 2) {
+                System.out.println("=[Transaction]=");
+                System.out.print("\nSelect: \n 1. Display \n 2. Deposit \n 3. Withdraw \n 4. Transfer\n Choose (0 to exit the program):");
+                choice = scanner.nextInt();
+                if (choice == 1) {
+                    System.out.print("To Display account Detail Please Enter Account Number : ");
+                    accNum = scanner.nextInt();
+                    for (i = 0; i < account.length; i++) {
+                        if (accNum == account[i].accNum) {
+                            System.out.println(account[i]);
+                            break;
+
+                        } else {
+                            System.out.println("Invalid Account Number");
+                            break;
+                        }
+                    }
+
+                } else if (choice == 2) {
+                    System.out.print("[Deposit]Please Enter Account Number : ");
+                    accNum = scanner.nextInt();
+                    for (i = 0; i < account.length; i++) {
+                        if (accNum == account[i].accNum) {
+                            System.out.println("Current Balance : RM " + account[i].balance);
+                            System.out.print("Deposit : RM ");
+                            balance = scanner.nextDouble();
+                            balance = balance + account[i].balance;
+                            account[i].setBalance(balance);
+                            System.out.println("New Balance : RM " + account[i].balance);
+                            break;
+
+                        } else {
+                            System.out.println("Invalid Account Number");
+                            break;
+                        }
+                    }
+                } else if (choice == 3){
+                    System.out.print("[Withdraw]Please Enter Account Number : ");
+                    accNum = scanner.nextInt();
+                    for (i = 0; i < account.length; i++) {
+                        if (accNum == account[i].accNum) {
+                            System.out.println("Current Balance : RM " + account[i].balance);
+                            System.out.print("Withdraw : RM ");
+                            balance = scanner.nextDouble();
+                            balance = account[i].balance - balance;
+                            account[i].setBalance(balance);
+                            System.out.println("New Balance : RM" + account[i].balance);
+                            break;
+
+                        } else {
+                            System.out.println("Invalid Account Number");
+                            break;
+                        }
+                    }
+                }
+            }
+
+        } while (choice != 0);
 
     }
 
