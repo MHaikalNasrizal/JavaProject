8e00c2740d456015b5e673d44ea737bd51c6f002
MHaikalNasrizal <haikalnasrizal321@gmail.com>
2022-12-03 12:45:04 +0800

    Bank Account Commit

--- JavaProject/src/javaproject/JavaProject.java
+++ JavaProject/src/javaproject/JavaProject.java
@@ -1,21 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package javaproject;
 
-/**
- *
- * @author Haikal
- */
+import java.util.Scanner;
+import java.util.Random;
 public class JavaProject {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        System.out.println("Test");
+        
+                
     }
+    
+}
+
+class BankAccount{
+    protected final String bankname = "ABC BANK";
+    protected String Fullname, type;
+    protected double interest,balance;
+
+    public BankAccount() {
+        
+    }
+    
     
 }
