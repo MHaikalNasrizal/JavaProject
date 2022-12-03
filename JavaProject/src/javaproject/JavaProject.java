53efe013f29fdf82aed64f4da401bade04d5619b
MHaikalNasrizal <haikalnasrizal321@gmail.com>
2022-12-03 23:09:00 +0800

    Exception Commit

--- JavaProject/src/javaproject/JavaProject.java
+++ JavaProject/src/javaproject/JavaProject.java
@@ -6,95 +6,145 @@ import java.util.Random;
 public class JavaProject {
 
     public static void main(String[] args) {
-        String Fullname, type;
-        double interest, balance;
+        String Fullname;
+        double balance;
         int accNum;
         int choice = 1;
         int i = 0;
+        int j = 0;
         BankAccount[] account = new BankAccount[50];
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         do {
+
             System.out.println("=== ABC BANK MAIN MENU ===");
             System.out.print("\nSelect : \n 1. Create Account\n 2. Transaction \n Choose (0 to exit the program): ");
             choice = scanner.nextInt();
 
             if (choice == 1) {
-                System.out.println("=[Create Account]=");
+                System.out.println("\n=[Create Account]=");
                 System.out.print("\nSelect: \n 1. Saving Account \n 2. Current Account\n Choose (0 to exit the program):");
                 choice = scanner.nextInt();
                 scanner.nextLine();
                 if (choice == 1) {
+
                     System.out.println("Enter your Full name : ");
                     Fullname = scanner.nextLine();
 
                     accNum = random.nextInt(9999 - 1001 + 1) + 1001;
-                    account[i] = new SavingAccount(Fullname, "Saving Account", accNum);
-                    System.out.println(account[i]);
-                    i++;
+                    account[j] = new SavingAccount(Fullname, "Saving Account", accNum);
+                    System.out.println(account[j]);
+                    j++;
+
                 } else if (choice == 2) {
+
                     System.out.println("Enter your Full name : ");
                     Fullname = scanner.nextLine();
 
                     accNum = random.nextInt(9999 - 1001 + 1) + 1001;
-                    account[i] = new CurrentAccount(Fullname, "Current Account", accNum);
-                    System.out.println(account[i]);
-                    i++;
+                    account[j] = new CurrentAccount(Fullname, "Current Account", accNum);
+                    System.out.println(account[j]);
+                    j++;
+
                 }
+
             } else if (choice == 2) {
                 System.out.println("=[Transaction]=");
                 System.out.print("\nSelect: \n 1. Display \n 2. Deposit \n 3. Withdraw \n 4. Transfer\n Choose (0 to exit the program):");
                 choice = scanner.nextInt();
-                if (choice == 1) {
-                    System.out.print("To Display account Detail Please Enter Account Number : ");
-                    accNum = scanner.nextInt();
-                    for (i = 0; i < account.length; i++) {
-                        if (accNum == account[i].accNum) {
-                            System.out.println(account[i]);
-                            break;
+                switch (choice) {
+                    case 1:
+                        System.out.print("To Display account Detail Please Enter Account Number : ");
+                        accNum = scanner.nextInt();
+                        try{
+                        for (i = 0; i < account.length; i++) {
+                            if (account[i].getAccNum() == accNum) {
+                                System.out.println(account[i]);
+                            }
 
-                        } else {
-                            System.out.println("Invalid Account Number");
-                            break;
                         }
-                    }
-
-                } else if (choice == 2) {
-                    System.out.print("[Deposit]Please Enter Account Number : ");
-                    accNum = scanner.nextInt();
-                    for (i = 0; i < account.length; i++) {
-                        if (accNum == account[i].accNum) {
-                            System.out.println("Current Balance : RM " + account[i].balance);
-                            System.out.print("Deposit : RM ");
-                            balance = scanner.nextDouble();
-                            balance = balance + account[i].balance;
-                            account[i].setBalance(balance);
-                            System.out.println("New Balance : RM " + account[i].balance);
-                            break;
-
-                        } else {
+                        }catch(NullPointerException e){
                             System.out.println("Invalid Account Number");
-                            break;
                         }
-                    }
-                } else if (choice == 3){
-                    System.out.print("[Withdraw]Please Enter Account Number : ");
-                    accNum = scanner.nextInt();
-                    for (i = 0; i < account.length; i++) {
-                        if (accNum == account[i].accNum) {
-                            System.out.println("Current Balance : RM " + account[i].balance);
-                            System.out.print("Withdraw : RM ");
-                            balance = scanner.nextDouble();
-                            balance = account[i].balance - balance;
-                            account[i].setBalance(balance);
-                            System.out.println("New Balance : RM" + account[i].balance);
-                            break;
+                        break;
 
-                        } else {
-                            System.out.println("Invalid Account Number");
-                            break;
+                    case 2:
+                        System.out.print("[Deposit]Please Enter Account Number : ");
+                        accNum = scanner.nextInt();
+                        for (i = 0; i < account.length; i++) {
+                            try{
+                            if (accNum == account[i].accNum) {
+                                System.out.println("Current Balance : RM " + account[i].getBalance());
+                                System.out.print("Deposit : RM ");
+                                balance = scanner.nextDouble();
+                                balance = balance + account[i].getBalance();
+                                account[i].setBalance(balance);
+                                System.out.println("New Balance : RM " + account[i].getBalance());
+                                break;
+
+                            }
+                            }catch(NullPointerException e){
+                                System.out.println("Invalid Account Number ");
+                            }
+
                         }
-                    }
+                        break;
+                    case 3:
+                        System.out.print("[Transfer]Please Enter Account Number : ");
+                        accNum = scanner.nextInt();
+                        try{
+                        for (i = 0; i < account.length; i++) {
+                            if (accNum == account[i].accNum) {
+                                System.out.println("Current Balance : RM " + account[i].getBalance());
+                                System.out.print("Withdraw : RM ");
+                                balance = scanner.nextDouble();
+                                balance = account[i].getBalance() - balance;
+                                account[i].setBalance(balance);
+                                System.out.println("New Balance : RM" + account[i].getBalance());
+                                break;
+
+                            }
+
+                        }
+                        }catch(NullPointerException e){
+                            System.out.println("Invalid Account Number");
+                        }
+                        break;
+                    case 4:
+                        System.out.print("[Transfer]Please Enter Account Number : ");
+                        accNum = scanner.nextInt();
+                        try{
+                        for (i = 0; i < account.length; i++) {
+                            if (accNum == account[i].accNum) {
+                                System.out.println("Current Balance : RM " + account[i].getBalance());
+                                System.out.print("Amount to transfer : RM ");
+                                balance = scanner.nextDouble();
+                                double send = balance;
+                                String sender = account[i].getFullname();
+                                balance = account[i].getBalance() - balance;
+                                account[i].setBalance(balance);
+                                System.out.print("[Transfer]Please Enter receiver Account Number : ");
+                                accNum = scanner.nextInt();
+                                for (i = 0; i < account.length; i++) {
+                                    if (accNum == account[i].accNum) {
+                                        balance = account[i].getBalance() + send;
+                                        account[i].setBalance(balance);
+                                        System.out.println("===Receipt===");
+                                        System.out.println("Sender name = " + sender);
+                                        System.out.println("Total Receive = " + send);
+                                        System.out.println("Account Number = " + account[i].getAccNum());
+                                        break;
+                                    }
+                                }break;
+                            }
+                         
+                        }
+                        }catch(NullPointerException e){
+                            System.out.println("Invalid Account Number");
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
 
@@ -104,21 +154,12 @@ public class JavaProject {
 
 }
 
-class BankAccount {
+abstract class BankAccount {
 
     protected final String bankname = "ABC BANK";
     protected String Fullname, type;
     protected double interest, balance;
     protected int accNum;
-
-    public BankAccount() {
-        Fullname = "Unknown";
-        type = "Unknown";
-        balance = 0.0;
-        interest = 0.0;
-        accNum = 0;
-
-    }
 
     public BankAccount(String Fullname, String type, int accNum) {
         this.Fullname = Fullname;
@@ -169,15 +210,13 @@ class BankAccount {
     }
 
     public String toString() {
-        return "BankAccount{" + "bankname=" + bankname + ", Fullname=" + Fullname + ", type=" + type + ", interest=" + interest + ", balance=" + balance + ", accNum=" + accNum + '}';
+        return "\n[BankAccount]" + "\nbankname=" + bankname + ", \nFullname=" + Fullname +
+                ", \ntype=" + type + ", \ninterest=" + interest + ", \nbalance=" + balance + ", \naccNum=" + accNum+"\n" ;
     }
 
 }
 
 class SavingAccount extends BankAccount {
-
-    public SavingAccount() {
-    }
 
     public SavingAccount(String Fullname, String type, int accNum) {
         super(Fullname, type, accNum);
@@ -188,9 +227,6 @@ class SavingAccount extends BankAccount {
 }
 
 class CurrentAccount extends BankAccount {
-
-    public CurrentAccount() {
-    }
 
     public CurrentAccount(String Fullname, String type, int accNum) {
         super(Fullname, type, accNum);
